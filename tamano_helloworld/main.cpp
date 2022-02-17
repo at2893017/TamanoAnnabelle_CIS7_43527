2f8da6f4e8000b038d65c826a5c640cb801f1f82
Annabelle Tamano <79181185+at2893017@users.noreply.github.com>
2022-02-17 13:08:52 -0800

    Add files via upload
    Hello World Program

---
+++ tamano_helloworld/main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: Annabelle Tamano
+ * Created on February 17, 2022, 1:05 PM
+ * Pupose: Hello World Program
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    cout<<"Hello, World!";
+    return 0;
+}
+
